f32c6d1d5db110dd8e957f8b265fe3a937ea31ab
maomao <tangpansak@gmail.com>
2022-11-02 22:05:00 +0700

    added pve login

--- vdiclient.py
+++ vdiclient.py
@@ -14,6 +14,9 @@ class VDIClient:
     hosts = [] # the host that we can select
     spice_proxy = {} # if we need proxying
     config = None # the config object for the app
+    proxmox = None
+    scaling = 1
+    logged_in = False
 
     def __init__(self, config_path):
         # loadconfig
@@ -27,6 +30,48 @@ class VDIClient:
         proxy = self.config.get("proxmox").get("SpiceProxyRedirect")
         if proxy:
             self.spice_proxy = proxy
+    def pve_auth(self, username, selected_host, passwd=None, totp=None):
+        print(self.hosts)
+        err = None
+        connected = False
+        authenticated = False
+        proxmox_conf = self.config.get("proxmox")
+        for hostinfo in self.hosts:
+            # set the selected host
+            if hostinfo["name"] == selected_host:
+                host = hostinfo['ip']
+                port = hostinfo['port']
+                try:
+                    # try token login if token exist
+                    token = proxmox_conf.get("token")
+                    backend = proxmox_conf.get("authentication").get("auth_backend")
+                    verify = proxmox_conf.get("authentication").get("tls_verify")
+                    if token:
+                        self.proxmox = proxmoxer.ProxmoxAPI(host, user=f'{username}@{backend}', token_name=token.get("name"),
+                                                         token_value=token.get("value"), verify_ssl=verify, port=port)
+                    # if the user is using totp
+                    elif totp:
+                        self.proxmox = proxmoxer.ProxmoxAPI(host, user=f'{username}@{backend}', otp=totp, password=passwd,
+                                                         verify_ssl=verify, port=port)
+                    # else login with username password
+                    else:
+                        self.proxmox = proxmoxer.ProxmoxAPI(host, user=f'{username}@{backend}', password=passwd,
+                                                         verify_ssl=verify, port=port)
+                    connected = True
+                    authenticated = True
+                    return connected, authenticated, err
+                # if can connect but cannot auth
+                except proxmoxer.backends.https.AuthenticationError as e:
+                    err = e
+                    connected = True
+                    return connected, authenticated, err
+                # unable to connect to url
+                except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectTimeout,
+                        requests.exceptions.ConnectionError) as e:
+                    err = e
+                    connected = False
+        return connected, authenticated, err
+
 
 if __name__ == "__main__":
     app = VDIClient(config_path="config.yaml")
