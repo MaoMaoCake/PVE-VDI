ae723e7f10b7a15970ebf129a5bc52a56ae71913
maomao <tangpansak@gmail.com>
2022-12-15 19:30:56 +0700

    created usertable sql

--- backend/tools/createdb.sql
+++ backend/tools/createdb.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE IF NOT EXISTS pve_vdi
+CREATE TABLE `users` (
+	`id` INT(16) NOT NULL AUTO_INCREMENT,
+	`username` varchar(255) NOT NULL UNIQUE,
+	`password` varchar(255) NOT NULL,
+	`salt` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL UNIQUE,
+	PRIMARY KEY (`id`)
+);
