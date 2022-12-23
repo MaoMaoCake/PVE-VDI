830a77afb1acadbb43355071b7edd1e24af115a8
maomao <tangpansak@gmail.com>
2022-12-23 20:29:48 +0700

    base authentication

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,13 +1,14 @@
 from pydantic import BaseModel
 
+class User(BaseModel):
+    username: str
+    CSRFToken: str = None
+    Ticket: str = None
+
 class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class TokenData(BaseModel):
     username: str | None = None
-
-class User(BaseModel):
-    user_id: int
-    username: str
-    role: str # Admin User Guest
