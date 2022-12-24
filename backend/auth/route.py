fba69a4fa6fd0b1c7f9db4fb1b7af9ab7eb2d9c5
maomao <tangpansak@gmail.com>
2022-12-24 18:41:21 +0700

    labeling of endpoints

--- backend/auth/route.py
+++ backend/auth/route.py
@@ -9,7 +9,7 @@ from .utils import create_access_token, authenticate_user, get_current_user, get
 authRouter = APIRouter()
 
 
-@authRouter.post("/token", response_model=Token)
+@authRouter.post("/token", response_model=Token, tags=["Auth"])
 async def login_for_access_token(form_data: PVEOAuth2PasswordRequestForm = Depends()) -> dict:
     """
     Takes in form data from the frontend and authenticates the user using the proxmox authentication api.
@@ -32,7 +32,7 @@ async def login_for_access_token(form_data: PVEOAuth2PasswordRequestForm = Depen
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@authRouter.get("/users/me/", response_model=User)
+@authRouter.get("/users/me/", response_model=User, tags=["Auth"])
 async def read_users_me(current_user: User = Depends(get_current_user)) -> User:
     """
     This is the endpoint for getting current user.
@@ -43,6 +43,6 @@ async def read_users_me(current_user: User = Depends(get_current_user)) -> User:
     return current_user
 
 
-@authRouter.get("/api/auth/realms")
+@authRouter.get("/api/auth/realms", tags=["Auth"])
 def pve_realm():
     return get_pve_realm()
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,30 @@
 from dotenv import load_dotenv
 load_dotenv('backend/backend.env')
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from .auth.route import authRouter
 from .pve.routes import pveRouter
 
-app = FastAPI()
+description = """
+PVE VDI is a VDI solution for proxmox
+"""
+
+tags_metadata = [
+    {
+        "name": "Auth",
+        "description": "Authentication Endpoint for interfacing with PVE",
+    },
+    {
+        "name": "PVE",
+        "description": "Proxmox Proxy Endpoints",
+    },
+]
+
+app = FastAPI(
+    title="PVE-VDI",
+    description=description,
+    version="0.0.1",
+    openapi_tags=tags_metadata
+)
 app.include_router(authRouter)
 app.include_router(pveRouter)
